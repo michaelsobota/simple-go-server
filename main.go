dd252a693bcdf06b611b90d9821f3f67bc25216e
Michael Sobota <mjsobota@gmail.com>
2019-05-24 22:55:09 -0600

    implementing default port logic

--- main.go
+++ main.go
@@ -7,10 +7,17 @@ func ping(w http.ResponseWriter, r *http.Request) {
   w.Write([]byte("pong"))
 }
 func main() {
-  port := os.Getenv("PORT")
+  port := getEnv("PORT", "80")
   http.Handle("/", http.FileServer(http.Dir("./http")))
   http.HandleFunc("/ping", ping)
   if err := http.ListenAndServe(":"+port, nil); err != nil {
     panic(err)
   }
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
